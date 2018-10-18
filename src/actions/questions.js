9859f4c65065c72c37e22d7ee6efd7cfb5209e48
jonsung89 <jonsung89@gmail.com>
2018-10-18 18:17:58 -0500

    Created actions and reducers

---
+++ src/actions/questions.js
@@ -0,0 +1,76 @@
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+
+export const FETCH_QUESTION_REQUEST = 'FETCH_QUESTION_REQUEST';
+export const fetchQuestionRequest = () => ({
+  type: FETCH_QUESTION_REQUEST
+});
+
+export const FETCH_QUESTION_SUCCESS = 'FETCH_QUESTION_SUCCESS';
+export const fetchQuestionSuccess = question => ({
+  type: FETCH_QUESTION_SUCCESS,
+  question
+});
+
+export const FETCH_QUESTION_ERROR = 'FETCH_QUESTION_ERROR';
+export const fetchQuestionError = error => ({
+  type: FETCH_QUESTION_ERROR,
+  error
+});
+
+export const VALIDATE_USER_INPUT_REQUEST = 'VALIDATE_USER_INPUT_REQUEST';
+export const validateUserInputRequest = () => ({
+  type: VALIDATE_USER_INPUT_REQUEST
+});
+
+export const VALIDATE_USER_INPUT_SUCCESS = 'VALIDATE_USER_INPUT_SUCCESS';
+export const validateUserInputSuccess = feedback => ({
+  type: VALIDATE_USER_INPUT_SUCCESS,
+  feedback
+});
+
+export const VALIDATE_USER_INPUT_ERROR = 'VALIDATE_USER_INPUT_ERROR';
+export const validateUserInputError = error => ({
+  type: VALIDATE_USER_INPUT_ERROR,
+  error
+});
+
+export const getQuestionData = () => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+
+  dispatch(fetchQuestionRequest());
+  return fetch(`${API_BASE_URL}/question`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => dispatch(fetchQuestionSuccess(data)))
+    .catch(err => dispatch(fetchQuestionError(err)));
+};
+
+export const validateUserInput = userInput => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+
+  dispatch(validateUserInputRequest());
+
+  return fetch(`${API_BASE_URL}/question`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    },
+    body: JSON.stringify({
+      answer: userInput
+    })
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => {
+      const feedback = data.msg;
+      dispatch(validateUserInputSuccess(feedback));
+    })
+    .catch(err => dispatch(validateUserInputError(err)));
+};
---
+++ src/reducers/questions.js
@@ -0,0 +1,63 @@
+import {
+  FETCH_QUESTION_REQUEST,
+  FETCH_QUESTION_SUCCESS,
+  FETCH_QUESTION_ERROR,
+  VALIDATE_USER_INPUT_REQUEST,
+  VALIDATE_USER_INPUT_SUCCESS,
+  VALIDATE_USER_INPUT_ERROR
+} from '../actions/questions';
+
+const initialState = {
+  question: {},
+  feedback: '',
+  answer: '',
+  userAnswered: false,
+  error: null,
+  loading: false
+};
+
+const fetchQuestion = (state = initialState, action) => {
+  if (action.type === FETCH_QUESTION_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+      error: null,
+      userAnswered: false
+    };
+  } else if (action.type === FETCH_QUESTION_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      question: action.question.question,
+      answer: action.question.question.answer
+    };
+  } else if (action.type === FETCH_QUESTION_ERROR) {
+    return {
+      ...state,
+      loading: false,
+      error: action.error
+    };
+  } else if (action.type === VALIDATE_USER_INPUT_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+      error: null
+    };
+  } else if (action.type === VALIDATE_USER_INPUT_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      feedback: action.feedback,
+      userAnswered: true
+    };
+  } else if (action.type === VALIDATE_USER_INPUT_ERROR) {
+    return {
+      ...state,
+      loading: false,
+      error: action.error
+    };
+  }
+  return state;
+};
+
+export default fetchQuestion;
